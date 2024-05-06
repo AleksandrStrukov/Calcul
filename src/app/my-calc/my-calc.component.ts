7d81a82464398de1f168d683b3e3e1d6df9441cb
Aleksandr <strukov_alexanderunner@hotmail.com>
2024-05-06 05:44:38 +0300

    hone 1

--- src/app/my-calc/my-calc.component.ts
+++ src/app/my-calc/my-calc.component.ts
@@ -1,15 +1,55 @@
 import {Component} from '@angular/core';
 
+interface CalcGroup {
+  first: CalcVar
+  second: CalcVar
+  operation: CalcOperations
+}
+
+interface CalcVar {
+  value: number
+  midificator: CulcModifies
+}
+
+enum CalcOperations {
+  'plus' = '+',
+  'minus' = '-',
+  'multiply' = '*',
+  'divide' = '/'
+}
+
+enum CulcModifies {
+  'cos' = 'cos',
+  'sin' = 'sin',
+  'square' = 'square',
+  'none' = 'none'
+}
+
+
 @Component({
   selector: 'app-my-calc',
   templateUrl: './my-calc.component.html',
   styleUrl: './my-calc.component.css'
 })
 export class MyCalcComponent {
-  public first: number = 1
-  public second: number = 1
-  public operation: string = '+'
-  public operations: string[] = ['+', '-', '*', '/']
+  public calcOperation = CalcOperations
+  public calcModification = CulcModifies
+  public calcGroups: CalcGroup[] = [
+    {
+      first: {
+        value: 5,
+        midificator: CulcModifies.none
+      },
+      second: {
+        value: 5,
+        midificator: CulcModifies.none
+      },
+      operation: CalcOperations.plus
+    }
+  ]
+   public history: string[] =[]
+  public operationsBeetweenGroup: CalcOperations[] = []
+
   public result?: number
 
   public calc() {
